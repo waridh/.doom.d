d546a681bbd4db0fe10e774789acfea6f1a112de
waridh <waridh@proton.me>
2024-04-11 20:17:56 -0600

    made the configuration more modern

--- config.el
+++ config.el
@@ -32,7 +32,9 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-gruvbox-light)
+(setq doom-theme 'doom-gruvbox)
+(setq doom-font (font-spec :family "Monoid Nerd Font Propo" :size 12))
+(set-frame-parameter (selected-frame) 'alpha '(95 . 95)) (add-to-list 'default-frame-alist '(alpha . (95 . 95)))
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
