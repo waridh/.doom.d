6fc20253b51803d7dada47d053f1c8ee56663c99
waridh <90401236+waridh@users.noreply.github.com>
2025-03-09 13:38:11 -0400

    swapped back to using corfu and vertico

--- init.el
+++ init.el
@@ -22,11 +22,11 @@
 
        :completion
        ;; (company  +tng)           ; the ultimate code completion backend
-       ;; (corfu +orderless +dabbrev +icons)  ; complete with cap(f), cape and a flying feather!
-       (helm +fuzzy +icons)              ; the *other* search engine for love and life
+       (corfu +orderless +icons +dabbrev)  ; complete with cap(f), cape and a flying feather!
+       ;; (helm +fuzzy +icons)              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
        ;; ivy               ; a search engine for love and life
-       ;; (vertico +icons)           ; the search engine of the future
+       (vertico +icons)           ; the search engine of the future
 
        :ui
        ;;deft              ; notational velocity for Emacs
