4347e3c65ef22f9edd52f5956e519e1d02a06d42
waridh <waridh.git@pm.me>
2025-08-10 08:31:31 -0400

    updted the current doom theme to doom one light

--- custom.el
+++ custom.el
@@ -5,7 +5,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("32f22d075269daabc5e661299ca9a08716aa8cda7e85310b9625c434041916af" default)))
+   '("5c7720c63b729140ed88cf35413f36c728ab7c70f8cd8422d9ee1cedeb618de5"
+     "32f22d075269daabc5e661299ca9a08716aa8cda7e85310b9625c434041916af" default)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
