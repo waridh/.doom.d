2c065fd49460afa814b1b0a9cd2abb21fe7f7b39
waridh <waridh.git@pm.me>
2025-08-10 12:53:58 -0400

    added scheme to the list of language I will work with

--- init.el
+++ init.el
@@ -166,7 +166,7 @@
        ;;(ruby +rails)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
        (rust +lsp +tree-sitter)       ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
-       ;; (scheme +guile +mit +racket)   ; a fully conniving family of lisps
+       (scheme +guile )   ; a fully conniving family of lisps
        (sh +tree-sitter +lsp +fish)                ; she sells {ba,z,fi}sh shells on the C xor
        ;;sml
        ;;solidity          ; do you need a blockchain? No.
