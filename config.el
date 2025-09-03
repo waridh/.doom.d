625303213c565759e564dd3c488c51480f4942b0
waridh <waridh.git@pm.me>
2025-09-03 11:36:18 -0600

    swapped back to a dark theme

--- config.el
+++ config.el
@@ -32,7 +32,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one-light)
+(setq doom-theme 'modus-vivendi-tritanopia)
 ;; (setq doom-font (font-spec :family "IntoneMono Nerd Font Mono" :size 14))
 
 ;; Dashboard modification
