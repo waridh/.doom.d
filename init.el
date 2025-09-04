8bb6a8ba66ace0bfb95cf50f5dba631dd5e3a963
waridh <waridh.git@pm.me>
2025-09-04 10:57:58 -0600

    updated the config to include graphviz

--- init.el
+++ init.el
@@ -137,6 +137,7 @@
        ;;gdscript          ; the language you waited for
        ;; (go +lsp +tree-sitter)         ; the hipster dialect
        ;;(graphql +lsp)    ; Give queries a REST
+       graphviz
        ;; (haskell +lsp +tree-sitter)    ; a language that's lazier than I am
        ;;hy                ; readability of scheme w/ speed of python
        ;;idris             ; a language you can depend on
